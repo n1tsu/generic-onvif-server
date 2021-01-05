462a1eddc3160c9059cca81a4b4b71a9c8153ba0
Augustin Thiercelin <augustin.thiercelin@procomm-mmc.com>
2021-01-05 18:10:24 +0100

    ptz: add to_gsoap for PTZNode class

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -26,6 +26,93 @@ int check_security(soap *soap)
 }
 
 
+tt__PTZNode *to_gsoap(soap *soap, PTZNode *node)
+{
+  auto g_node = soap_new_tt__PTZNode(soap);
+
+  g_node->token = node->get_token();
+  g_node->Name = soap_new_tt__Name(soap);
+  *g_node->Name = node->get_name();
+  g_node->MaximumNumberOfPresets = node->max_presets;
+  g_node->HomeSupported = node->support_home;
+  g_node->FixedHomePosition = soap_new_bool(soap);
+  *g_node->FixedHomePosition = node->fixed_home;
+
+  g_node->SupportedPTZSpaces = soap_new_tt__PTZSpaces(soap);
+
+  // ABSOLUTE
+  auto pan_tilt_abs = soap_new_tt__Space2DDescription(soap);
+  pan_tilt_abs->URI = "http://www.onvif.org/ver10/tptz/PanTiltSpaces/PositionGenericSpace";
+  pan_tilt_abs->XRange = soap_new_tt__FloatRange(soap);
+  pan_tilt_abs->XRange->Min = node->absolute_ranges.pan_min;
+  pan_tilt_abs->XRange->Max = node->absolute_ranges.pan_max;
+  pan_tilt_abs->YRange = soap_new_tt__FloatRange(soap);
+  pan_tilt_abs->YRange->Min = node->absolute_ranges.tilt_min;
+  pan_tilt_abs->YRange->Max = node->absolute_ranges.tilt_max;
+  g_node->SupportedPTZSpaces->AbsolutePanTiltPositionSpace.push_back(pan_tilt_abs);
+
+  auto zoom_abs = soap_new_tt__Space1DDescription(soap);
+  zoom_abs->URI = "http://www.onvif.org/ver10/tptz/ZoomSpaces/PositionGenericSpace";
+  zoom_abs->XRange = soap_new_tt__FloatRange(soap);
+  zoom_abs->XRange->Min = node->absolute_ranges.zoom_min;
+  zoom_abs->XRange->Max = node->absolute_ranges.zoom_max;
+  g_node->SupportedPTZSpaces->AbsoluteZoomPositionSpace.push_back(zoom_abs);
+
+  // RELATIVE
+  auto pan_tilt_rel = soap_new_tt__Space2DDescription(soap);
+  pan_tilt_rel->URI = "http://www.onvif.org/ver10/tptz/PanTiltSpaces/TranslationGenericSpace";
+  pan_tilt_rel->XRange = soap_new_tt__FloatRange(soap);
+  pan_tilt_rel->XRange->Min = node->relative_ranges.pan_min;
+  pan_tilt_rel->XRange->Max = node->relative_ranges.pan_max;
+  pan_tilt_rel->YRange = soap_new_tt__FloatRange(soap);
+  pan_tilt_rel->YRange->Min = node->relative_ranges.tilt_min;
+  pan_tilt_rel->YRange->Max = node->relative_ranges.tilt_max;
+  g_node->SupportedPTZSpaces->RelativePanTiltTranslationSpace.push_back(pan_tilt_rel);
+
+  auto zoom_rel = soap_new_tt__Space1DDescription(soap);
+  zoom_rel->URI = "http://www.onvif.org/ver10/tptz/ZoomSpaces/TranslationGenericSpace";
+  zoom_rel->XRange = soap_new_tt__FloatRange(soap);;
+  zoom_rel->XRange->Min = node->relative_ranges.zoom_min;
+  zoom_rel->XRange->Max = node->relative_ranges.zoom_max;
+  g_node->SupportedPTZSpaces->RelativeZoomTranslationSpace.push_back(zoom_rel);
+
+  // CONTINUOUS
+  auto pan_tilt_con = soap_new_tt__Space2DDescription(soap);
+  pan_tilt_con->URI = "http://www.onvif.org/ver10/tptz/PanTiltSpaces/VelocityGenericSpace";
+  pan_tilt_con->XRange = soap_new_tt__FloatRange(soap);
+  pan_tilt_con->XRange->Min = node->continuous_ranges.pan_min;
+  pan_tilt_con->XRange->Max = node->continuous_ranges.pan_max;
+  pan_tilt_con->YRange = soap_new_tt__FloatRange(soap);
+  pan_tilt_con->YRange->Min = node->continuous_ranges.tilt_min;
+  pan_tilt_con->YRange->Max = node->continuous_ranges.tilt_max;
+  g_node->SupportedPTZSpaces->ContinuousPanTiltVelocitySpace.push_back(pan_tilt_rel);
+
+  auto zoom_con = soap_new_tt__Space1DDescription(soap);
+  zoom_con->URI = "http://www.onvif.org/ver10/tptz/ZoomSpaces/VelocityGenericSpace";
+  zoom_con->XRange = soap_new_tt__FloatRange(soap);
+  zoom_con->XRange->Min = node->continuous_ranges.zoom_min;
+  zoom_con->XRange->Max = node->continuous_ranges.zoom_max;
+  g_node->SupportedPTZSpaces->ContinuousZoomVelocitySpace.push_back(zoom_rel);
+
+  // SPEED
+  auto pan_tilt_speed = soap_new_tt__Space1DDescription(soap);
+  pan_tilt_speed->URI = "http://www.onvif.org/ver10/tptz/PanTiltSpaces/GenericSpeedSpace";
+  pan_tilt_speed->XRange = soap_new_tt__FloatRange(soap);
+  pan_tilt_speed->XRange->Min = node->speed_ranges.pan_min;
+  pan_tilt_speed->XRange->Max = node->speed_ranges.pan_max;
+  g_node->SupportedPTZSpaces->PanTiltSpeedSpace.push_back(pan_tilt_speed);
+
+  auto zoom_speed = soap_new_tt__Space1DDescription(soap);
+  zoom_speed->URI = "http://www.onvif.org/ver10/tptz/ZoomSpaces/ZoomGenericSpeedSpace";
+  zoom_speed->XRange = soap_new_tt__FloatRange(soap);
+  zoom_speed->XRange->Min = node->speed_ranges.zoom_min;
+  zoom_speed->XRange->Max = node->speed_ranges.zoom_max;
+  g_node->SupportedPTZSpaces->ZoomSpeedSpace.push_back(zoom_speed);
+
+  return g_node;
+}
+
+
 tt__VideoSourceConfiguration *to_gsoap(soap *soap, std::shared_ptr<VideoConfiguration> configuration)
 {
   auto video_configuration = soap_new_tt__VideoSourceConfiguration(soap);
--- src/utils/utils.h
+++ src/utils/utils.h
@@ -10,6 +10,8 @@ std::string last_occurence(std::string str, std::string separators);
 
 int check_security(soap *soap);
 
+tt__PTZNode *to_gsoap(soap *soap, PTZNode *node);
+
 tt__PTZConfiguration *to_gsoap(soap *soap, std::shared_ptr<PTZConfiguration> configuration);
 
 tt__VideoEncoderConfiguration *to_gsoap(soap *soap, std::shared_ptr<EncoderConfiguration> configuration);
